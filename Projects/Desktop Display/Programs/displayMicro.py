fd9f13370ed89b4d60a537e93ad9154322404a1a
Bradley Burkard <166866940+BBHydraOC@users.noreply.github.com>
2026-02-11 10:23:13 -0500

    Add nessecary import
    Microcontroller based implementation not working properly due to missing import. Fixed.

--- Projects/Desktop Display/Programs/displayMicro.py
+++ Projects/Desktop Display/Programs/displayMicro.py
@@ -1,11 +1,12 @@
 from sys import platform
+import serial
 
 if platform == "linux":
     from pcSensorsLin import cpuload, gpuload, ramload, vramload
 elif platform == "win32":
     from pcSensorsWin import cpuload, gpuload, ramload, vramload
     
-ser = serial.Serial("COM14", 115200)  # change COM port if needed (Work on auto com port designation later)
+ser = serial.Serial("COM10", 115200)  # change COM port if needed (Work on auto com port designation later)
 while True:
     cpu = cpuload()
     gpu = gpuload()
