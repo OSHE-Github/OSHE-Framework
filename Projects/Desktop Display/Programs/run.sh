8c7ecc9085e994351da5bbb7d6e5982a57d146c0
BBHydraOC <166866940+BBHydraOC@users.noreply.github.com>
2026-02-24 12:47:02 -0500

    Patch linux

--- Projects/Desktop Display/Programs/run.sh
+++ Projects/Desktop Display/Programs/run.sh
@@ -2,5 +2,10 @@
 python3 -m venv ~/py_envs
 source ~/py_envs/bin/activate
 
-python3 display.py
+pip install easymcp2221
+pip install psutil
+pip install pyopencl
+pip install pyserial
+
+python3 displayI2C.py
 
