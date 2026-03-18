2c2308b900224733b22dd972de443361e5264423
Bradley Burkard <166866940+BBHydraOC@users.noreply.github.com>
2026-03-18 14:02:36 -0400

    Merged changes to old files
    Maintain modularity and also implement auto display detection on the PC side.

--- Projects/Desktop Display/Programs/displayMicro.py
+++ Projects/Desktop Display/Programs/displayMicro.py
@@ -1,12 +1,28 @@
 from sys import platform
 import serial
+from time import sleep
+import serial.tools.list_ports
 
 if platform == "linux":
     from pcSensorsLin import cpuload, gpuload, ramload, vramload
+    wait = 0.5
 elif platform == "win32":
     from pcSensorsWin import cpuload, gpuload, ramload, vramload
+    wait = 0
     
-ser = serial.Serial("COM10", 115200)  # change COM port if needed (Work on auto com port designation later)
+#Port autodetection yay!
+target_name = "OSHE Framework Desktop Compatible Display"
+ports = serial.tools.list_ports.comports()
+port = ""
+
+for port in ports:
+    if target_name in port.description:
+        port = {port.device}
+
+if port == "":
+    print(f"ERROR: No display detected")
+
+ser = serial.Serial("/dev/ttyACM0", 115200)  # change COM port if needed (Work on auto com port designation later)
 while True:
     cpu = cpuload()
     gpu = gpuload()
@@ -16,3 +32,5 @@ while True:
     # Send data to MicroController
     data = f"{cpu},{ram},{gpu},{vram}\n"
     ser.write(data.encode())
+    sleep(wait)
+
